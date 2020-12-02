87420cb969e66c74f9135f33ea2f6689ec49d1eb
AaronBison <37123069+AaronBison@users.noreply.github.com>
2020-12-02 19:27:26 +0200

    Failed navigation bar implementation

--- app/src/main/java/com/example/andoridproject/SplashScreenFragment.kt
+++ app/src/main/java/com/example/andoridproject/SplashScreenFragment.kt
@@ -5,15 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.navigation.Navigation
+import com.example.andoridproject.R
 
 class SplashScreenFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_splash_screen, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_splash_screen, container, false)
 
+        var containerLayouts = view.findViewById<LinearLayout>(R.id.containerLayout)
+        containerLayouts.setOnClickListener{
+            Navigation.findNavController(view).navigate(R.id.action_splashScreenFragment_to_mainScreenFragment)
+        }
+
+        return view
+    }
 }
