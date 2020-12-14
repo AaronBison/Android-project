3c2a5802e187f62b759ab1c4f436ad9a9580440b
AaronBison <37123069+AaronBison@users.noreply.github.com>
2020-12-14 19:49:49 +0200

    Pick image from gallery

--- app/src/main/java/com/example/andoridproject/fragments/modify/ModifyFragment.kt
+++ app/src/main/java/com/example/andoridproject/fragments/modify/ModifyFragment.kt
@@ -1,19 +1,29 @@
 package com.example.andoridproject.fragments.modify
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import androidx.annotation.Nullable
+import androidx.core.provider.FontsContractCompat.FontRequestCallback.RESULT_OK
+import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
 import com.example.andoridproject.R
-import kotlinx.android.synthetic.main.fragment_detail_screen.view.*
+import kotlinx.android.synthetic.main.fragment_modify.*
 import kotlinx.android.synthetic.main.fragment_modify.view.*
 
+
 class ModifyFragment : Fragment() {
+
+    private val GALLERY_REQUEST_CODE = 123
+
+    private lateinit var imageView: ImageView
 
     private val args by navArgs<ModifyFragmentArgs>()
 
@@ -32,16 +42,57 @@ class ModifyFragment : Fragment() {
             .centerCrop()
             .placeholder(R.drawable.image_placeholder)
             .into(view.modifyImageView)
+
         view.modifyNameEditText.setText(args.modifiableRestaurant.name)
         view.modifyAddressEditText.setText(args.modifiableRestaurant.address)
         view.modifyPriceEditText.setText(args.modifiableRestaurant.price)
 
 
+        // Pick image from gallery
+        imageView = view.findViewById(R.id.modifyImageView)
+        view.changeImageButton.setOnClickListener {
+            val intent = Intent()
+            intent.type = "image/*"
+            intent.action = Intent.ACTION_GET_CONTENT
+            startActivityForResult(
+                Intent.createChooser(intent, "Pick an image"),
+                GALLERY_REQUEST_CODE
+            )
+        }
+
         view.applyButton.setOnClickListener{
+            updateRestaurant()
+
             val action = ModifyFragmentDirections.actionModifyFragmentToFragmentDetailScreen(args.modifiableRestaurant)
             Navigation.findNavController(view).navigate(action)
         }
 
+
         return view
+    }
+
+
+    override fun onActivityResult(
+        requestCode: Int,
+        resultCode: Int,
+        @Nullable data: Intent?
+    ) {
+
+        // Set imageView after image picked
+        if (requestCode == GALLERY_REQUEST_CODE && data != null) {
+            val imageData: Uri? = data.data
+            imageView.setImageURI(imageData)
+        }
+    }
+
+    private fun updateRestaurant(){
+        val name = modifyNameEditText.text
+        val address = modifyAddressEditText.text
+        val price = modifyPriceEditText.text
+
+//        val image_url = modifyImageView
+
+//        Log.e("IMAGE","$image_url")
+
     }
 }
